768863bfc9c59a169cb1e0473bab2d10ae86899e
OKharchenko <OKharchenko@luxoft.com>
2022-07-20 20:55:51 +0300

    Logic Implemented

---
+++ src/main/java/com/example/reactivenasalargestpicfinder/ReactiveNasaLargestPicFinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactivenasalargestpicfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveNasaLargestPicFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveNasaLargestPicFinderApplication.class, args);
+    }
+
+}
